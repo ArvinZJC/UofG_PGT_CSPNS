a6e6fb305b814ced1a9814451324858f89fc5b5e
ArvinZJC <tomzjc@qq.com>
2021-11-26 18:46:57 +0000

    Improve evaluation.

--- eval.py
+++ eval.py
@@ -1,11 +1,11 @@
 """
 '''
 Description: the utilities of evaluation
-Version: 2.0.0.20211125
+Version: 2.0.0.20211126
 Author: Arvin Zhao
 Date: 2021-11-21 14:50:13
 Last Editors: Arvin Zhao
-LastEditTime: 2021-11-25 23:33:07
+LastEditTime: 2021-11-26 18:45:55
 '''
 """
 
@@ -48,7 +48,7 @@ class Eval:
     def __make_cwnd_plot(
         self, base_dir: str, colours: np.ndarray, experiments: list, name: str
     ) -> None:
-        """Make a plot indicating the flow CWND over time.
+        """Make a plot indicating CWND over time.
 
         Parameters
         ----------
@@ -62,10 +62,12 @@ class Eval:
             The name of an experiment for an AQM algorithm to compare with the baseline.
         """
         info(
-            f"*** Plotting the baseline and the AQM algorithm's flow CWND over time: {self.__FLOW_1} - {GROUP_B} - {self.__BW_NAME_DEFAULT} - {name}\n"
+            f"*** Plotting the baseline and the AQM algorithm's CWND over time: {self.__FLOW_1} - {GROUP_B} - {self.__BW_NAME_DEFAULT} - {name}\n"
         )
         plt.figure()
-        plt.title("CWND over time")
+        plt.title(
+            f"CWND over time: {'CoDel' if name == self.__CODEL else name.upper()}"
+        )
 
         for experiment, colour in zip(experiments, colours):
             if experiment == self.__TBF or experiment == name:
@@ -83,7 +85,7 @@ class Eval:
         plt.savefig(os.path.join(base_dir, f"cwnd_{name}.png"))
 
     def __make_rtt_plot(self, base_dir: str, bw_name: str, experiments: list) -> None:
-        """Make a plot indicating the flow RTT over time.
+        """Make a plot indicating RTT over time.
 
         Parameters
         ----------
@@ -94,20 +96,17 @@ class Eval:
         experiments : list
             A list of experiment names.
         """
-        info(
-            f"*** Plotting each AQM algorithm's RTT over time: {self.__FLOW_1} - {GROUP_B} - {bw_name}\n"
-        )
+        info(f"*** Plotting RTT over time: {self.__FLOW_1} - {GROUP_B} - {bw_name}\n")
         plt.figure()
         plt.title("RTT over time")
 
         for experiment in experiments:
-            if experiment != self.__TBF and experiment != self.__SFQ:
-                data = pd.read_csv(
-                    os.path.join(base_dir, experiment, "hl1", self.__file_formatted),
-                    header=None,
-                    sep=" ",
-                )[:-1][[0, 3]]
-                plt.plot(data[0], data[3], label=experiment)
+            data = pd.read_csv(
+                os.path.join(base_dir, experiment, "hl1", self.__file_formatted),
+                header=None,
+                sep=" ",
+            )[:-1][[0, 3]]
+            plt.plot(data[0], data[3], label=experiment)
 
         plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
         plt.xlabel("time (s)")
@@ -121,10 +120,9 @@ class Eval:
         bw_name: str,
         colours: np.ndarray,
         experiments: list,
-        has_baseline: bool,
         has_sfq_only: bool,
     ) -> None:
-        """Make a plot indicating the flow throughput over time.
+        """Make a plot indicating throughput over time.
 
         Parameters
         ----------
@@ -136,33 +134,27 @@ class Eval:
             A customised colour map.
         experiments : list
             A list of experiment names.
-        has_baseline : bool
-            A flag indicating if the baseline should be on the plot.
         has_sfq_only : bool
             A flag indicating if SFQ is the only AQM algorithm on the plot.
         """
         info(
             "*** Plotting "
-            + (
-                "the baseline and SFQ"
-                if has_baseline
-                else ("SFQ" if has_sfq_only else "each AQM algorithm")
-            )
-            + f"'s flow throughput over time: {self.__FLOW_2} - {GROUP_B} - {bw_name}\n"
+            + ("the baseline and SFQ's " if has_sfq_only else "")
+            + f"throughput over time: {self.__FLOW_2} - {GROUP_B} - {bw_name}"
+            + (f" - {self.__SFQ}" if has_sfq_only else "")
+            + "\n"
         )
         plt.figure()
         plt.title(
-            f"Throughput over time{'' if has_baseline else ': SFQ'}"
+            f"Throughput over time: {self.__SFQ.upper()}"
             if has_sfq_only
             else "Fairness"
         )
 
         for experiment, colour in zip(experiments, colours):
             if (
-                (has_baseline and experiment in [self.__TBF, self.__SFQ])
-                or (not has_baseline and has_sfq_only and experiment == self.__SFQ)
-                or (not has_baseline and not has_sfq_only and experiment != self.__TBF)
-            ):
+                has_sfq_only and experiment in [self.__SFQ, self.__TBF]
+            ) or not has_sfq_only:
                 for i in range(2):
                     data = pd.read_csv(
                         os.path.join(
@@ -178,23 +170,19 @@ class Eval:
                         label=experiment if i == 0 else None,
                     )
 
-        if has_baseline == has_sfq_only:
-            plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
-            plt.tight_layout()
-
+        plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
         plt.xlabel("time (sec)")
         plt.ylabel("throughput (Mbps)")
+        plt.tight_layout()
         plt.savefig(
             os.path.join(
                 base_dir,
-                "throughput.png"
-                if has_baseline
-                else ("throughput_sfq.png" if has_sfq_only else "fairness.png"),
-            )
+                f"throughput_{self.__SFQ}.png" if has_sfq_only else "fairness.png",
+            ),
         )
 
     def plot_cwnd(self) -> None:
-        """Plot the flow CWND over time for the group transferring data for the specified time length with 1 flow and the default bandwidth."""
+        """Plot CWND over time for the group transferring data for the specified time length with 1 flow and the default bandwidth."""
         base_dir = os.path.join(
             self.__base_dir, self.__FLOW_1, GROUP_B, self.__BW_NAME_DEFAULT
         )
@@ -209,7 +197,7 @@ class Eval:
             )
 
     def plot_fct(self) -> None:
-        """Plot all FCT for the group transferring the specified amount of data with 1 flow and the default bandwidth."""
+        """Plot FCT for the group transferring the specified amount of data with 1 flow and the default bandwidth."""
         base_dir = os.path.join(
             self.__base_dir, self.__FLOW_1, GROUP_A, self.__BW_NAME_DEFAULT
         )
@@ -225,7 +213,7 @@ class Eval:
             for experiment in experiments
         ]
         info(
-            f"*** Plotting all FCT: {self.__FLOW_1} - {GROUP_A} - {self.__BW_NAME_DEFAULT}\n"
+            f"*** Plotting FCT: {self.__FLOW_1} - {GROUP_A} - {self.__BW_NAME_DEFAULT}\n"
         )
         plt.figure()
         plt.title("FCT achieved in each experiment")
@@ -239,7 +227,7 @@ class Eval:
         plt.savefig(os.path.join(base_dir, "fct.png"))
 
     def plot_rtt(self) -> None:
-        """Plot the flow RTT over time for the group transferring data for the specified time length with 1 flow and different bandwidth settings."""
+        """Plot RTT over time for the group transferring data for the specified time length with 1 flow and different bandwidth settings."""
         group_base_dir = os.path.join(self.__base_dir, self.__FLOW_1, GROUP_B)
         bw_names = [
             entry.name for entry in os.scandir(group_base_dir) if entry.is_dir()
@@ -248,18 +236,14 @@ class Eval:
         for bw_name in bw_names:
             base_dir = os.path.join(group_base_dir, bw_name)
             experiments = sorted(
-                [
-                    entry.name
-                    for entry in os.scandir(base_dir)
-                    if entry.is_dir() and entry.name != self.__TBF
-                ]
+                [entry.name for entry in os.scandir(base_dir) if entry.is_dir()]
             )
             self.__make_rtt_plot(
                 base_dir=base_dir, bw_name=bw_name, experiments=experiments
             )
 
     def plot_throughput(self) -> None:
-        """Plot the flow throughput over time for the group transferring data for the same time length with 2 flows and different bandwidth settings."""
+        """Plot throughput over time for the group transferring data for the same time length with 2 flows and different bandwidth settings."""
         group_base_dir = os.path.join(self.__base_dir, self.__FLOW_2, GROUP_B)
         bw_names = [
             entry.name for entry in os.scandir(group_base_dir) if entry.is_dir()
@@ -272,29 +256,25 @@ class Eval:
             )
             colours = plt.cm.jet(np.linspace(0, 1, len(experiments)))
 
-            for has_baseline, has_sfq_only in zip(
-                [False, True, False], [True, True, False]
-            ):
+            for has_sfq_only in [True, False]:
+                if not has_sfq_only and bw_name != self.__BW_NAME_DEFAULT:
+                    continue
+
                 self.__make_throughput_plot(
                     base_dir=base_dir,
                     bw_name=bw_name,
                     colours=colours,
                     experiments=experiments,
-                    has_baseline=has_baseline,
                     has_sfq_only=has_sfq_only,
                 )
 
     def plot_utilisation(self) -> None:
-        """Plot each AQM algorithm's link utilisation for the group transferring data for the specified time length with 1 flow and the default bandwidth."""
+        """Plot link utilisation for the group transferring data for the specified time length with 1 flow and the default bandwidth."""
         base_dir = os.path.join(
             self.__base_dir, self.__FLOW_1, GROUP_B, self.__BW_NAME_DEFAULT
         )
         experiments = sorted(
-            [
-                entry.name
-                for entry in os.scandir(base_dir)
-                if entry.is_dir() and entry.name != self.__TBF
-            ]
+            [entry.name for entry in os.scandir(base_dir) if entry.is_dir()]
         )
         results = [
             pd.read_csv(
@@ -307,7 +287,7 @@ class Eval:
             for experiment in experiments
         ]
         info(
-            f"*** Plotting each AQM algorithm's link utilisation: {self.__FLOW_1} - {GROUP_B} - {self.__BW_NAME_DEFAULT}\n"
+            f"*** Plotting link utilisation: {self.__FLOW_1} - {GROUP_B} - {self.__BW_NAME_DEFAULT}\n"
         )
         plt.figure()
         plt.title("Link utilisation")
@@ -315,7 +295,7 @@ class Eval:
         for experiment, result in zip(experiments, results):
             plt.bar(experiment, result)
 
-        plt.xlabel("AQM algorithm")
+        plt.xlabel("experiment")
         plt.ylabel("link utilisation (%)")
         plt.ylim(np.min(results) - 5, 100)
         plt.savefig(os.path.join(base_dir, "utilisation.png"))
